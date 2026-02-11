9c425bb9738da290b006a3eb79a3be7ded9e1507
Adou-83 <adluc1983@gmail.com>
2026-02-11 13:23:45 +0000

    Add humanize to installed apps

--- restaurantplus/settings.py
+++ restaurantplus/settings.py
@@ -23,7 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'django.contrib.humanize',  # <-- Ajoute cette ligne
     # Tes apps
     'core',
 ]
